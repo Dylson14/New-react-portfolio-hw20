e0aade2c79512d479cf4e3fba431b0d2e00a1e4f
Dylson Oliveira <dylsonuni@gmail.com>
2022-02-05 20:33:43 +0000

    re-naming components to lowercase

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,1 +1,11 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
+
+import LandingPage from './landingpage'
+
+
+const Main = () => {
+    <Switch>
+         <Route exact path="/" component={LandingPage} />
+    </Switch>
+}
