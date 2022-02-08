b85c028fa51f78c4475ac71d766996726cbb995d
Dylson Oliveira <dylsonuni@gmail.com>
2022-02-08 12:09:17 +0000

    creating the banner css beneath the avatar

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,5 +1,30 @@
 import React from "react";
+import {Container, Row, Col} from 'react-bootstrap';
+import avatar from '../assets/dylson.jpg'
+
 
 export default function Landing() {
-    return <h1>This is the Landing page</h1>;
+    return (
+        <div>
+            <Container fluid className="landing-grid">
+                <Row>
+                    <Col></Col>
+                    <Col><img src={avatar} alt="picture of Dylson" className="avatar-img"/></Col>
+                    <Col></Col>
+                </Row>
+            </Container>
+
+            <Container fluid className="landing-grid">
+            <div className="banner-text">
+            <h1> Full-Stack Web Developer </h1>
+            <hr />
+            </div>
+
+            <p>HTML/CSS | Bootstrap | Javascrip</p>
+            </Container>
+            
+
+        </div>
+    );
+
 }
