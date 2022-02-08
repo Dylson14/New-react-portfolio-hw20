b044d9502c81bc3561a010bd2d506381a6619774
Dylson Oliveira <dylsonuni@gmail.com>
2022-02-08 12:09:50 +0000

    adding gradint color to the app.css for the header avatar and grid

--- src/App.js
+++ src/App.js
@@ -10,33 +10,31 @@ function App() {
 
     return (
       <div>
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-    <Container>
-    <Navbar.Brand href="/home">Dylson's Portfolio</Navbar.Brand>
-    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-    <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="me-auto">
-        <Link to="/resume">Resume</Link>
-        <Link to="/about">About Me</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/contact">Contact</Link>
-      </Nav>
-    </Navbar.Collapse>
-    </Container>
-        </Navbar>
-      </div>
-    );
-  
-  render(
-  // Unsure why this approach works and the other approach does not. 
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>,
+        <div>
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className='header-color'>
+      <Container>
+      <Navbar.Brand href="/">Dylson's Portfolio</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Link to="/resume">Resume</Link>
+          <Link to="/about">About Me</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/contact">Contact</Link>
+        </Nav>
+      </Navbar.Collapse>
+      </Container>
+          </Navbar>
+        </div>
 
-    document.getElementById("root")
+        <div>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+    
+</div>
   );
 }
 
