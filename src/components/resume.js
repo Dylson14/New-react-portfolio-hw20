5d4e0e0c5c3f14d4120f1dbe0eb93736d8f2a60f
Dylson Oliveira <dylsonuni@gmail.com>
2022-02-09 19:16:52 +0000

    created the button to retrieve CV

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,6 +1,12 @@
 import React from "react";
 
 export default function Resume() {
-    return <h1>Find my Resume here</h1>;
+  return (
+    <div>
+      <h2>Click the button below to retrieve professional CV</h2>
+      <form action="https://www.linkedin.com/in/dylsonoliveira/overlay/1635479032466/single-media-viewer/">
+    <input type="submit" value="Get CV" />
+</form>
+    </div>
+  );
 }
-
